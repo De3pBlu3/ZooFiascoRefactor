435e3630154b81fee38811c1f226cf403c978534
23357614 <jackcasey614@gmail.com>
2023-11-23 16:36:54 +0000

    added getters and setters

--- Animal.java
+++ Animal.java
@@ -1,5 +1,37 @@
 // Animal.java
 public class Animal {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String[] getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(String[] favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
     private String name;
     private String type;
     private String[] favoriteFoods;
