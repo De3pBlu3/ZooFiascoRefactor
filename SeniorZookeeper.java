68fc29cf2c41f06963430595af761cd5f033ca83
23357614 <jackcasey614@gmail.com>
2023-11-23 16:43:49 +0000

    Changed all zooKeepers to use getters instead of direct access

--- SeniorZookeeper.java
+++ SeniorZookeeper.java
@@ -6,14 +6,14 @@ public class SeniorZookeeper {
     }
 
     public void feedAnimal(Animal animal) {
-        System.out.println(name + " is feeding " + animal.name);
+        System.out.println(name + " is feeding " + animal.getName());
     }
 
     public void cleanCage(Animal animal) {
-        System.out.println(name + " is cleaning " + animal.type + "'s cage");
+        System.out.println(name + " is cleaning " + animal.getType() + "'s cage");
     }
 
     public void giveMedicine(Animal animal) {
-        System.out.println(name + " is giving medicine to " + animal.type);
+        System.out.println(name + " is giving medicine to " + animal.getType());
     }
 }
