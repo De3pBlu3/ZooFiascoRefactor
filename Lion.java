6ad2092ea33f8c992c73ebd7d2887dfd56458beb
unknown <23371323@studentmail.ul.ie>
2023-11-23 17:10:47 +0000

    MORE UPDATES

--- Lion.java
+++ Lion.java
@@ -1,7 +1,7 @@
 public class Lion extends Animal {
 
     public Lion(String name, String[] favoriteFoods, String sound) {
-        super(name, "lion", favoriteFoods, sound);
+        super(name, "lion", favoriteFoods);
     }
 
     @Override
@@ -21,7 +21,7 @@ public class Lion extends Animal {
 
     @Override
     public void performTrick(){
-        System.out.println(getName() + "the" + getType() + " is performing a trIck");
+        System.out.println(getName() + "the" + getType() + " is performing a trick");
         System.out.println("It JUMPS and SPINS and does a big ROAR!");
     }
 }
